a88d2f3e975a8bf25a28f2251fe8f92788899549
zyzstart <1738091590@qq.com>
2023-12-07 13:27:13 +0800

    新增菜品

---
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -0,0 +1,19 @@
+package com.sky.mapper;
+
+import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author zyzstart
+ * @creat 2023-12-07 1:49
+ */
+@Mapper
+public interface DishFlavorMapper {
+    /**
+     * 批量插入口味数据
+     * @param flavors
+     */
+    void insertBatch(List<DishFlavor> flavors);
+}
---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,11 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+
+/**
+ * @author zyzstart
+ * @creat 2023-12-07 1:24
+ */
+public interface DishService {
+    public void saveWithFlavor(DishDTO dishDTO);
+}
