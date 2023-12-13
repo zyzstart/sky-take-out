4dadbc1c5a61461b2f3ef89fa0de391e94933b1f
zyzstart <1738091590@qq.com>
2023-12-13 23:49:00 +0800

    添加购物车代码开发

---
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -0,0 +1,12 @@
+package com.sky.service;
+
+import com.sky.dto.ShoppingCartDTO;
+
+/**
+ * @author zyzstart
+ * @creat 2023-12-13 21:58
+ */
+public interface ShoppingCartService {
+
+    void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+}
